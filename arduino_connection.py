b8d5d421e9149fd5b797a4eb33fff915702f01ef
fsahli <pi@raspberrypi.(none)>
2014-02-07 19:25:16 +0000

    Bug Fixes

--- arduino_connection.py
+++ arduino_connection.py
@@ -1,14 +1,31 @@
 import serial
-
-slope = 0.038107675647276
-intercept = -1.42243458142442
-def connect_to_Arduino():
-	
-	ser = serial.Serial('/dev/ttyUSB0',9600)
-	return ser
-
-def read_weight(ser):
-	ser.write('1')
-	adc = int(ser.readline().rstrip())
-	weight = slope*adc+intercept
-	return weight
+import time
+path = '/home/pi/RCerezas_v1/'
+class Arduino:
+	def __init__(self):
+		fo = open(path+'data/intercept.txt','r')
+		self.intercept = float(fo.read())
+		fo.close()
+		self.slope = 0.038107675647276
+		self.ser = serial.Serial('/dev/ttyUSB0',9600)
+		self.ser.write('1')
+		self.asking = False
+		self.last_weight = 0
+	def read_weight(self):
+#		self.asking=True
+		self.ser.write('1')
+		adc = int(self.ser.readline().rstrip())
+#		self.asking = False
+		weight = self.slope*adc+self.intercept
+		print weight
+		self.last_weight = weight
+		return weight
+	def set_zero(self):
+#		self.asking=True
+		self.ser.write('1')
+		adc = int(self.ser.readline().rstrip())
+#		self.asking=False
+		self.intercept = -self.slope*adc
+		fo = open(path+"data/intercept.txt", "w")
+		fo.write(str(self.intercept))
+		fo.close()
--- beta1.py
+++ beta1.py
@@ -6,86 +6,127 @@ from datetime import datetime
 from readtest import readCard
 from csv_gspread import *
 import os.path
-from  arduino_connection import *
+from  arduino_connection import Arduino
 import thread
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
+delay = 30
+path='/home/pi/RCerezas_v1/'
 if not os.geteuid() == 0:
     sys.exit('Script must be run as root')
 
 ON, OFF = [1, 0]
 
+asking_serial = False
 
-#def Int_shutdown(channel):  
-#	print "Boton detectado"
+def Int_shutdown(channel):  
+	print "Shutdown"
+	ard.ser.write('2')
+	os.system("sudo shutdown -h now")
+	lcd.cls()
+	lcd.gotorc(1,0)
+	lcd.text("Apagando")
+	sys.exit("Apagando")
+def Int_shutdown2(channel):
+	print "Zero"
+	while ard.asking:
+		time.sleep(0.05)
+	ard.set_zero()
 
-
-def display_weight(ser, lcd):
+def retrieve_weight(ard):
+	print "Retrive Weight thread"
 	while 1:
-		time.sleep(1)
-		lcd.gotorc(5,0)
-		lcd.text("%d.2 kgs" % read_weight(ser))
-		time.sleep(1)
+		ard.read_weight()
+		time.sleep(0.1)
+def display_weight(ard,lcd):
+	print "Display Weight Thread"
+	time.sleep(1)
+	while 1:
+		if not ard.asking:
+			lcd.gotorc(5,0)
+			lcd.text("%.2f kgs" % ard.last_weight)
+			time.sleep(0.5)
 
 try:
-	#GPIO.setmode(GPIO.BCM) 
-	#GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
-	#GPIO.add_event_detect(25, GPIO.FALLING, callback = Int_shutdown, bouncetime = 2000)
-	lcd.init()
-	lcd.cls()
-	lcd.set_contrast(180)
-	lcd.backlight(OFF)
+	try:
+		print 'Iniciando botones'
+		GPIO.setmode(GPIO.BCM) 
+		GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+		GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+		GPIO.add_event_detect(25, GPIO.FALLING, callback = Int_shutdown, bouncetime = 2000)
+		GPIO.add_event_detect(22, GPIO.FALLING, callback = Int_shutdown2, bouncetime = 1000)
+	except:
+		print 'Error al iniciar botones'
+	try:
+		print 'Iniciando LCD'
+		lcd.init()
+		lcd.cls()
+		lcd.set_contrast(180)
+		lcd.backlight(OFF)
+	except:
+		print 'Error al iniciar LCD'
+
 	lcd.centre_text(0,"Verificando Internet...")
+	print "Verificando Internet"
 	if internet_on():
 		lcd.cls()
 		lcd.centre_text(0,"Bajando Registro de nombres...")
-		dowloadNombreTarjeta()
+		print "Bajando registro de nombres"
+		try:
+			dowloadNombreTarjeta()
+		except:
+			print "No se pudo bajar registro"
+	else:
+		lcd.cls()
+		lcd.centre_text(0,"No hay internet...")
+		print "No hay internet"
+	if os.path.isfile(path+'data/NombreTarjeta.csv'):
+		lcd.cls()
+		lcd.centre_text(0,"Ocupando registro...")
+		print "Ocupando registro"
 		cardID_Name = create_cardID_Name_dict()
 		cardID_RUT = create_cardID_RUT_dict()
 	else:
 		lcd.cls()
-		lcd.centre_text(0,"No hay internet...")
-		if os.path.isfile('data/NombreTarjeta.csv'):
-			lcd.cls()
-			lcd.centre_text(0,"Ocupando registro antiguo...")
-			cardID_Name = create_cardID_Name_dict()
-			cardID_RUT = create_cardID_RUT_dict()
-		else:
-			lcd.cls()
-			lcd.centre_text(0,"No hay registro de nombres")
-			sys.exit('No hay registro de nombres')
+		lcd.centre_text(0,"No hay registro de nombres")
+		sys.exit('No hay registro de nombres')
 #Verifica si el existe el archivo IDlastbin.txt y si no lo crea con un cero
-	if not os.path.isfile('data/IDlastbin.txt'):
-		fo = open("data/IDlastbin.txt", "w")
+	if not os.path.isfile(path+'data/IDlastbin.txt'):
+		fo = open(path+"data/IDlastbin.txt", "w")
 		fo.write("0")
 		fo.close()
 #Verifica si el existe el archivo IDlastbin_uploaded.txt y si no lo crea con un cero
-	if not os.path.isfile('data/IDlastbin_uploaded.txt'):
-		fo = open("data/IDlastbin_uploaded.txt", "w")
+	if not os.path.isfile(path+'data/IDlastbin_uploaded.txt'):
+		fo = open(path+"data/IDlastbin_uploaded.txt", "w")
 		fo.write("0")
 		fo.close()
 #Se conecta al arduino		
-	ser=connect_to_Arduino()	
-	print ser
-	thread.start_new_thread( display_weight ,( ser, lcd, ) )
+	try:
+		ard=Arduino()	
+		print ard.ser
+		thread.start_new_thread( retrieve_weight ,(ard, ) )
+		thread.start_new_thread( display_weight ,(ard, lcd, ) )
+	except Exception,e:
+		print 'Error al iniciar conexion serial'
+		print str(e)
 	while 1:
 	#Lee desde el archivo la variable IDlastbin	
-		fo = open("data/IDlastbin.txt", "r")
+		fo = open(path+"data/IDlastbin.txt", "r")
 		IDbin = int(fo.read())+1
 		fo.close()
 	#Lee desde el archivo la variable IDlastbin_uploaded	
-		fo = open("data/IDlastbin_uploaded.txt", "r")
+		fo = open(path+"data/IDlastbin_uploaded.txt", "r")
 		IDlastbin_uploaded = int(fo.read())
 		fo.close()
 #Verifica si es quedo un archivo temp sin guardar en bin.
-		if os.path.isfile('data/temp.csv'):
-			os.rename('data/temp.csv','data/Bin%d.csv' % IDbin)
-			fo = open("data/IDlastbin.txt", "w")
+		if os.path.isfile(path+'data/temp.csv'):
+			os.rename(path+'data/temp.csv',path+'data/Bin%d.csv' % IDbin)
+			fo = open(path+"data/IDlastbin.txt", "w")
 			fo.write(str(IDbin))
 			fo.close()
 			IDbin+=1
 	#Crea el archivo temp.csv
-		ftemp = open("data/temp.csv", "w")
+		ftemp = open(path+"data/temp.csv", "w")
 		ftemp.write("Bin %d\n" % IDbin)
 		ftemp.close()
 		registro = []
@@ -108,23 +149,25 @@ try:
 				diff=(ahora-last_caja_timestamp).total_seconds()
 				kgs=sum([cajas[i][1] for i in range(len(cajas))])
 			else:
-				diff=130
+				diff=delay+10
 				kgs=0
-			if diff<120:
+			if diff<delay:
 				lcd.cls()
-				lcd.centre_text(0,"Tienes que esperar 2 min. antes de la proxima caja")
+				lcd.centre_text(0,"Tienes que esperar %d seg antes de la proxima caja" % delay)
 #Registra la marca en la lista registro y en el archivo temp.
 			else:
 				weight_aux=0.0
-				weight = read_weight(ser)
-				while (abs(weight-weight_aux)>0.2 and weight>2.0):
+				weight = ard.last_weight
+				while (abs(weight-weight_aux)>0.1 and weight<2.0):
 					weight_aux=weight
-					weight = read_weight(ser)
-					time.sleep(0.1)
+					weight = ard.last_weight
+					time.sleep(0.12)
+				#	lcd.gotorc(5,0)
+				#	lcd.text("%.2f kgs" % ard.read_weight())
 				timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 				registro.append([timestamp,cardID,cardID_RUT.get(cardID,'No asignado'),weight])
-				ftemp = open("data/temp.csv", "a")
+				ftemp = open(path+"data/temp.csv", "a")
 				ftemp.write(timestamp+","+cardID+","+cardID_RUT.get(cardID,'No asignado')+","+str(weight)+"\n")
 				ftemp.close()
 				user_count = len(cajas)+1
@@ -136,20 +179,24 @@ try:
 				lcd.text("Cajas: "+str(user_count))
 				nroCajas+=1
 				lcd.gotorc(4,0)
-				lcd.text("kg: "+str(kgs+weight))
+				lcd.text("kg: %.2f" % (kgs+weight))
 #				lcd.centre_text(4,"%d/24 cajas" % nroCajas)
 				print cardID
 				print nroCajas
 #Cuando se termina el bin o se pone la tarjeta nuevobin se mueve el archivo temp.csv a BinXX.csv. Se actualiza el ID del ultimo bin.
-		os.rename('data/temp.csv','data/Bin%d.csv' % IDbin)
-		fo = open("data/IDlastbin.txt", "w")
+		os.rename(path+'data/temp.csv',path+'data/Bin%d.csv' % IDbin)
+		fo = open(path+"data/IDlastbin.txt", "w")
 		fo.write(str(IDbin))
 		fo.close()
 		
 except KeyboardInterrupt:
   pass
+except Exception,e: 
+	print str(e)
 finally:
-  lcd.cls()
-  lcd.backlight(OFF)
-  ser.close()
-
+	lcd.cls()
+	lcd.backlight(OFF)
+	try:
+		ard.ser.close()
+	except:
+		print "No se pudo cerrar con. serial"
